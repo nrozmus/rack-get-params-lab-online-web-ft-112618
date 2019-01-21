94075b0f5d99551d4a703ef8006dff7b1f1360cf
Nicholas Rozmus <rozmusnicholas@gmail.com>
2019-01-21 23:36:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -11,6 +12,9 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+      elsif req.path.match(/cart/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
